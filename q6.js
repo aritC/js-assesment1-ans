a168c0260331cebc4288f74dace24edf69e56e58
Arit Chanda <aritchanda1234@gmail.com>
2023-08-02 10:44:47 -0700

    answers for q6

--- q6.js
+++ q6.js
@@ -1,1 +1,15 @@
 //Q6. Print the topics you remember so far exporting it from one file and printing in another
+
+const fs = require("fs");
+var topics = require("./topics.json");
+console.log(topics);
+
+const topicText = JSON.stringify(topics);
+
+fs.writeFile("./exportedTopics.json", topicText, "utf8", function (err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("The file was saved!");
+});
